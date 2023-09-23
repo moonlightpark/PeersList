746f83b5d36336da45a04963ffcde2a0c9068cc5
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-09-23 21:44:34 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+전체 노드를 찾아서 노드 보상 주소를 크롤링 한 후, 일괄 정제 시킨다
+'''
+import pprint
+import pandas as pd
+import json
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = urlopen('http://main.saseul.net/peer')
+bsObject = BeautifulSoup(url, "html.parser")
+
+f = open('./peer_original_data.txt', 'w')
+f.write(str(bsObject))
+f.close()
+
+file = open('./peer_original_data.txt')
+js = json.load(file)
+res = json.dumps(js)
+file.close()
+
+text = pprint.pformat(json.loads(res))
+#f = open('./peers.json', 'w')
+#f.write(str(text))
+#f.close()
+
+#
+###########################
+#
+file_path = './peers.json'
+with open(file_path,encoding='UTF-8') as json_file:
+    data = json.load(json_file)
+    print(type(data))
+
+for i in data['peers']:
+    print(data['peers'][i]['address'])
